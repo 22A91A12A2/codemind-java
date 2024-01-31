42553f198549582f730c78690329962463d77758
Kanchu Harish <119840058+kanchuharish24@users.noreply.github.com>
2024-01-31 10:24:19 +0530

    import java.util.*; public class inout{     public static void main(String args[])     {         Scanner h=new Scanner(System.in);         String s=h.nextLine();         System.out.println(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class inout{
+    public static void main(String args[])
+    {
+        Scanner h=new Scanner(System.in);
+        String s=h.nextLine();
+        System.out.println(s);
+    }
+}
