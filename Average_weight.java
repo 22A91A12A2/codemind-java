ed602f34fed368a6ca56cf7d5d0c817f8642e210
Kanchu Harish <119840058+kanchuharish24@users.noreply.github.com>
2024-01-31 09:47:59 +0530

    import java.util.*; public class weight{     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a,b,c,avg;        a=sc.nextInt();        b=sc.nextInt();        c=sc.nextInt();        avg=3*a-(b+c);        System.out.println(avg);     } }

---
+++ Average_weight.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class weight{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a,b,c,avg;
+       a=sc.nextInt();
+       b=sc.nextInt();
+       c=sc.nextInt();
+       avg=3*a-(b+c);
+       System.out.println(avg);
+    }
+}
