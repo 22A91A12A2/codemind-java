b401b581f47a3dfb290c39a89aca3a72abc6f4d4
Kanchu Harish <119840058+kanchuharish24@users.noreply.github.com>
2024-01-31 10:28:58 +0530

    import java.util.*; public class inout{     public static void main(String args[])     {         Scanner h=new Scanner(System.in);         int a=h.nextInt();         int b=h.nextInt();         int c=a/b;         System.out.println(c); } }

---
+++ Division.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class inout{
+    public static void main(String args[])
+    {
+        Scanner h=new Scanner(System.in);
+        int a=h.nextInt();
+        int b=h.nextInt();
+        int c=a/b;
+        System.out.println(c);
+}
+}
