29c17d0d6e55cc49c516345d2c1a040102283881
Kanchu Harish <119840058+22A91A12A2@users.noreply.github.com>
2024-02-14 09:57:46 +0530

    import java.util.*; public class tickets{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         if(x<=250)         {             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Tickets.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class tickets{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x<=250)
+        {
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
