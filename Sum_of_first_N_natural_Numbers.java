02c5c9588503aa43964a31ecd04bf7551e35e15c
Kanchu Harish <119840058+22A91A12A2@users.noreply.github.com>
2024-02-14 11:41:24 +0530

    import java.util.*; public class tickets{     public static void main(String []args){         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int c=x*(x+1)/2;
                System.out.println(c);
            }
        }

---
+++ Sum_of_first_N_natural_Numbers.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class tickets{
+    public static void main(String []args){
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int c=x*(x+1)/2;
+        
+            System.out.println(c);
+        }
+    }
