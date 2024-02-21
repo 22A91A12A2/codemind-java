af3d5bc95b6c09487f437963aac1f1086e2bb18c
Kanchu Harish <119840058+22A91A12A2@users.noreply.github.com>
2024-02-21 09:41:45 +0530

    import java.util.*; public class frames{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int c=sc.nextInt();         int p=a+b;         int z=2*(p*c);         System.out.println(z);     } }

---
+++ Frames.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class frames{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        int p=a+b;
+        int z=2*(p*c);
+        System.out.println(z);
+    }
+}
